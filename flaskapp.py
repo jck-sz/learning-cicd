d808d56ac9f09ea5c7a79f0a0af5cfae4188a596
Jacek <jacek.szlaczka@gmail.com>
2025-04-15 18:18:04 +0200

    Added Dockerfile and requirements.txt

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, world! xdd"
+    return "Hello, world! Test123"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1337, debug=True)
